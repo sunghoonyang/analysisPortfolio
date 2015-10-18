f2b9189bb3c7eb982717dc6c459966ee275ccaed
sunghoonyang <sunghoonyang@github.com>
2015-10-18 13:09:28 -0700

    typo with the cleaned data's name

--- foodWorldCup/cleanUp.py
+++ foodWorldCup/cleanUp.py
@@ -1,32 +1,32 @@
 DATA = 	"./food-world-cup-data.csv"
-OUTPUT = './phytonCleaned.csv'
+OUTPUT = './pythonCleaned.csv'
 HEADER = './header.csv'
 cleanData = open(OUTPUT, 'w')
-
+#writing column headers
 with open(HEADER) as fileobj:
-	for word in fileobj:
-		row = word.split(',')
+	for line in fileobj:
+		headers = line.split(',')
 		i = 0
-		for cell in row:
-			if "Please rate how much you like the traditional cuisine of" in cell:
-				cell = cell.rsplit(None, 1)[-1][:-1]
-
-			cleanData.write(cell + "\t")
-			i+=1
-			if i == 48:
-				i = 0
-				cleanData.write("\n")
-
+		for col in headers:
+			if "Please rate how much you like the traditional cuisine of" in col:
+				col = col.rsplit(None, 1)[-1][:-1]
+			i += 1
+			if i < 48:
+				cleanData.write(col + ",")
+			else:
+				cleanData.write(col + "\n")
+#let's write the data now
 with open(DATA) as fileobj:
 	for word in fileobj:
 		row = word.split(',')
 		i = 0
 		for cell in row:
-			cleanData.write(cell + "\t")
 			i+=1
-			if i == 48:
+			if i > 48:
 				i = 0
-				cleanData.write("\n")
+				cleanData.write(cell + "\n")
+			else:
+				cleanData.write(cell + ",")
 
 cleanData.close()
 
