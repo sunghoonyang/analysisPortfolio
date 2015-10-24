21b9b385e2a426239a9691210d346dff9876c16e
sunghoonyang <sunghoonyang@github.com>
2015-10-24 16:50:19 -0700

    policeKillings first commit

---
+++ policeKilling/source.R
@@ -0,0 +1,9 @@
+#########################
+#police killing analysis#
+#########################
+library(readr)
+library(ggplot2)
+dt <- read.csv('/Users/Abraxas/Documents/analysisPortfolio/policeKilling/data/police_killings.csv')
+#I'm going to use census tract as the geographical unit
+#Things that may have association with police killings are: 
+#armed?, age, gender, raceethnicity, month,day,tract, unemployment, poverty, and education (college)
