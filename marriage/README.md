99fb32118a17663672dd17ca0c579e6dbf0891a1
sunghoonyang <sunghoonyang@github.com>
2015-10-30 20:19:18 -0700

    correcting the visuals, deleting the NA's in the visuals, and README.md

--- marriage/README.md
+++ marriage/README.md
@@ -1,14 +1,13 @@
 <h1>Are You Sleeping Alone?</h1>
 <h3>Understanding the Population</h3>
-<img src = "./img/mosaic_marriage_income.png">
+<img src = "./img/mosaic_status_income.png">
 <h4>Marriage Status vs. Household Income</h4>
 <img src = "./img/mosaic_status_duration.png">
 <h4>Marriage Status vs. Length of Relationship</h4>
-
-
-<img src = "./img/ageVSintimacy.png">
-<h4>Age vs. Sleeping Intimacy</h4>
+<p>Married couples exhibit longer duration of marriage. DUH. So if "in a relationship" people's data would be more revealing if we compare it to that of "out of relationship" people, if we pivot around duration
 <img src = "./img/everyNight_incomeVSgender.png">
 <h4>Income Gender mosaic for people who sleep seperately everyday</h4>
 <img src = "./img/Never_incomeVSgender.png">
 <h4>Income Gender mosaic for people who never sleep seperately</h4>
+<img src = "./img/ageVSintimacy.png">
+<h4>Age vs. Sleeping Intimacy</h4>
--- marriage/source.R
+++ marriage/source.R
@@ -32,14 +32,33 @@ df$income <- as.factor(df$income)
 levels(df$income) <- c("NA", "<25k", "<150k", "150k<", "<50k", "<100k")
 df$income <- factor(df$income, levels(df$income)[c(1,2,5,6,3,4)])
 
-#people who sleep with the partners seperately everyday
-noSuccess <- subset(df, df[,"freqSeperate"] == "Every night" | df[,"freqSeperate"] = "A few times per week")
-#count vs. their age
-ggplot(noSuccess, aes(noSuccess$age)) + 
-  geom_histogram() +
-  ggtitle("Age distribution for people who sleep on\n seperate beds at least few times per week")
-#mosaic of income vs. gender
+#mosaic_status_income.png & mosaic_status_duration
+dt <- subset(df, df[, "income"] != "NA")
+dt <- subset(dt, dt[, "duration"] != "NA")
+dt <- drop.levels(dt) 
+dt$income <- factor(dt$income, levels(dt$income)[c(2,3,4,5,1)])
+mosaic(~ status + income, dt)
+dt$duration <- factor(dt$duration, levels(dt$duration)[c(2,3,4,5,1)])
+mosaic(~ status + duration, dt)
+
+
+#making mosaics; everyNight_incomeVSgender  & Never_incomeVSgender
 Never <- subset(df, df[,"freqSeperate"] == "Never")
-ggplot(Never, aes(Never$age)) + 
-  geom_histogram() +
-  ggtitle("Age distribution for people who sleep together")
+EveryNight <- subset(df, df[,"freqSeperate"] == "Every night")
+Never <- subset(Never, Never[, "gender"] != "")
+Never <- subset(Never, Never[, "income"] != "NA")
+EveryNight <- subset(EveryNight, EveryNight[, "gender"] != "")
+EveryNight <- subset(EveryNight, EveryNight[, "income"] != "NA")
+Never <- drop.levels(Never) 
+EveryNight <- drop.levels(EveryNight) 
+mosaic(~ gender + income, Never)
+mosaic(~ gender + income, EveryNight)
+
+#df - surveys with no response
+totalResponse <- subset(df, df[,"freqSeperate"] != "NA") 
+totalResponse <- subset(totalResponse, totalResponse[,"age"] != "")
+ggplot(totalResponse, aes(totalResponse$age, fill = totalResponse$freqSeperate)) +
+  geom_bar() +
+  guides(fill=guide_legend(reverse=TRUE)) +
+  ggtitle("Age versus sleeping intimacy")
+
