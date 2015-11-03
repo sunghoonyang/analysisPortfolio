15c6a784a2e19d671225e80be3efd2ec2e012447
sunghoonyang <sunghoonyang@github.com>
2015-11-03 14:21:54 -0800

    source code for drug and age cleaned up

--- drugAndAge/source.R
+++ drugAndAge/source.R
@@ -2,7 +2,11 @@ library(readr)
 library(ggplot2)
 library(vcd)
 dt <- read_csv("/Users/Abraxas/documents/analysisPortfolio/drugAndAge/data/drug-use-by-age.csv")
-dt[dt == "-"] <- NA
+#change characters to numeric
+for (i in seq(from = 3, to = 28)) {
+  if (class(dt[, i]) == "character") dt[, i] <- as.numeric(dt[, i])
+}
+dt[dt == "-"] <- 0
 gg_names <- c("alcohol", "marijuana", "cocaine", "crack", "heroin", "hallucinogen", "inhalant", "pain-reliever", "oxycotin", "tranquilizer", "stimulant", "meth", "sedative")
 for (i in seq( from =1, to = length(gg_names) ) ) {
     png(paste0("/Users/Abraxas/Documents/analysisPortfolio/drugAndAge/img/", gg_names[i], ".png"))
@@ -19,13 +23,3 @@ for (i in seq( from =1, to = length(gg_names) ) ) {
     print(a)
     dev.off()
 }  
-a <- ggplot(dt, aes(x = age, y = dt$`alcohol-use`)) +
-  geom_point(aes(size = dt$`alcohol-frequency` )) +
-  geom_smooth(method = "auto", aes(group = 1)) +
-  theme(plot.title = element_text(face="bold", colour="#000000", size=14)) + 
-  ggtitle(paste("Age and", gg_names[1], "use by frequency\n", sep = " ")) +
-  theme(axis.title.x = element_text(face="bold", colour="#000000", size=14), axis.text.x  = element_text(angle=90, vjust=0.5, size=16)) + 
-  xlab("Age") +
-  theme(axis.title.y = element_text(face="bold", colour="#000000", size=14), axis.text.y  = element_text(angle=90, vjust=0.5, size=16)) +
-  ylab("Substance use (% of pop.)\n") +
-  scale_size_continuous(guide = guide_legend(title = "Frequency"))
